5b63a5336222f9aa30ba5b032de679d960860e7e
Trirrin <2628883576@qq.com>
2025-12-01 17:40:38 +0800

    chore(version): bump version to 1.0.5-beta-1 and update changelog

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ fun String.runCommand(): String {
     }
 }
 
-val appVersionCode = 6
-val appVersionName = "1.0.4"
+val appVersionCode = 7
+val appVersionName = "1.0.5-beta-1"
 val commitHash = "git rev-parse --short=8 HEAD".runCommand().ifEmpty { "unknown" }
 val fullVersionName = "v${appVersionName}(${appVersionCode})(commit:${commitHash})"
 
