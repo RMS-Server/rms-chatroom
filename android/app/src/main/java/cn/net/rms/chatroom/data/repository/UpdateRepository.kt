38962e33758b577a785d3695361b36cc034f7428
Trirrin <2628883576@qq.com>
2025-12-30 17:53:24 +0800

    fix(android): Add missing import for GitHubReleaseResponse in UpdateRepository

--- android/app/src/main/java/cn/net/rms/chatroom/data/repository/UpdateRepository.kt
+++ android/app/src/main/java/cn/net/rms/chatroom/data/repository/UpdateRepository.kt
@@ -14,6 +14,7 @@ import androidx.core.content.FileProvider
 import cn.net.rms.chatroom.BuildConfig
 import cn.net.rms.chatroom.data.api.ApiService
 import cn.net.rms.chatroom.data.api.AppUpdateResponse
+import cn.net.rms.chatroom.data.api.GitHubReleaseResponse
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
